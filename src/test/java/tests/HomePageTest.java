c92bb21b7f3069e392e697ee14cd02566dbaa218
himani-sharma <himani.sharma@games24x7.com>
2023-08-22 17:51:40 +0530

    fixed report portal

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -2,24 +2,24 @@ package tests;
 
 import org.oneframework.allureReport.TestListener;
 import org.oneframework.imageCompare.ImageComparator;
+import org.oneframework.logger.LoggerUtil;
+import org.oneframework.logger.LoggingManager;
 import org.oneframework.pageObjects.HomePage;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.logging.Logger;
 
 @Listeners({TestListener.class})
 public class HomePageTest extends BaseTest {
 
-   private static Logger LogManager;
-    private static final Logger LOGGER = LogManager.getLogger(String.valueOf(HomePageTest.class));
+   private static final LoggingManager log = new LoggingManager(HomePageTest.class.getName());
 
     SoftAssert softAssert = new SoftAssert();
 
     @Test(description = "testing the signin page visually")
     public void testSignInOption() throws Exception {
-        LOGGER.info("Running testSignInOption");
+        log.debug("Running testSignInOption");
         System.out.println("Running testSignInOption");
         HomePage homePage = new HomePage(driverThread.get());
         softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("homePage"), "homePage baseline image isn't matching with actual image.");
