85ca5625a11244f6a6d221fe2d8994dfa8008b06
himani-sharma <himani.sharma@games24x7.com>
2023-08-23 09:53:11 +0530

    uncommented hardcoded asserts

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -19,24 +19,26 @@ public class HomePageTest extends BaseTest {
 
     @Test(description = "testing the signin page visually")
     public void testSignInOption() throws Exception {
-        Assert.assertTrue(false);
+       // Assert.assertTrue(false);
         log.debug("Running testSignInOption");
         System.out.println("Running testSignInOption");
-//        HomePage homePage = new HomePage(driverThread.get());
-//        softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("homePage"), "homePage baseline image isn't matching with actual image.");
-//        homePage.chooseSignInOption();
-//        softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("signinPage"), "signinPage baseline image isn't matching with actual image.");
-//        softAssert.assertAll();
+        HomePage homePage = new HomePage(driverThread.get());
+       softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("homePage"), "homePage baseline image isn't matching with actual image.");
+       homePage.chooseSignInOption();
+       softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("signinPage"), "signinPage baseline image isn't matching with actual image.");
+       softAssert.assertAll();
     }
 
     @Test(description = "testing the signup page visually")
     public void testSignUpOption() throws Exception {
-        Assert.assertTrue(false);
+        log.debug("Running testSignUpOption");
+
+        // Assert.assertTrue(false);
         System.out.println("Running testSignUpOption");
-//        HomePage homePage = new HomePage(driverThread.get());
-//        homePage.chooseSignUpOption();
-//        softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("signupPage"), "signupPage baseline image isn't matching with actual image.");
-//        softAssert.assertAll();
+       HomePage homePage = new HomePage(driverThread.get());
+        homePage.chooseSignUpOption();
+       softAssert.assertTrue(new ImageComparator(driverThread.get()).compare("signupPage"), "signupPage baseline image isn't matching with actual image.");
+      softAssert.assertAll();
     }
 
 }
